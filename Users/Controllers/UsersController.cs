fc791decb1c6e45312bae5151c7e5ca84f2aa636
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2019-12-10 11:51:00 -0600

    Enrutamiento de atributos Parte 3

--- Users/Controllers/UsersController.cs
+++ Users/Controllers/UsersController.cs
@@ -11,28 +11,35 @@ namespace Users.Controllers
     //[Route("Usuarios/")]
     public class UsersController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("[controller]/[action]/{data:double}")]
+        public IActionResult Index(double data)
         {
-            return View();
+            return View("Index", data);
         }
 
-        //[HttpPost]
-        //[HttpGet]
-        // Estas son las rutas que nuestra accion puede utlizar
-        [Route("/Usuarios/Hugo")]
-        //[Route("/Usuarios/Enrique/{data}")]
-        //[Route("[action]/{data}")]
-        [Route("[controller]/[action]/{data}/")]
-        public IActionResult Index(string data, int age)
-        {
-            // Estos parametros se pasan por la url y son parametros
-            // distintos a los declarados
-            //ViewData["id"] = data + " " + age;
+        ////[HttpPost]
+        ////[HttpGet]
+        //// Estas son las rutas que nuestra accion puede utlizar
+        //[Route("/Usuarios/Hugo")]
+        ////[Route("/Usuarios/Enrique/{data}")]
+        ////[Route("[action]/{data}")]
+        ////[Route("[controller]/[action]/{data}/")]
+        //[HttpGet("[controller]/[action]/{data:int}")]
+        //public IActionResult Index(int data)
+        //{
+        //    // Estos parametros se pasan por la url y son parametros
+        //    // distintos a los declarados
+        //    //ViewData["id"] = data + " " + age;
 
-            // Instanciamos un nuevo objeto, el cual tambien puede ser pasado a la
-            // vista el emtodo sobrecargado de view, que acepta una vista y un modelo(objeto)
-            String datos = data + " " + age;
-            return View("Index", datos);
-        }
+        //    // Instanciamos un nuevo objeto, el cual tambien puede ser pasado a la
+        //    // vista el emtodo sobrecargado de view, que acepta una vista y un modelo(objeto)
+        //    //String datos = data;
+        //    return View("Index", data);
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
     }
 }
